326213a8330dc79cbdba5bbadfe1a71a3128e304
Ankush Jaiswal <ankushjaiswal1111@gmail.com>
2024-06-16 17:18:45 +0530

    frontend complete

---
+++ src/Components/Page4/Page4Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page4Text = () => {
+  return (
+    <div className='absolute z-9 top-[20vh]  left-[50vw] translate-x-[-50%]'>
+        <h1 className='text-5xl text-center font-semibold'>Blogs</h1>
+        <p className='text-white bg-black m-2 	'>Get Inspiration for your next trip</p>
+    </div>
+)
+}
+
+export default Page4Text
